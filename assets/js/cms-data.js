3e4a0bc55d5c7ec05990204af775527d90b39814
Kimani Waweru <kimaniwaweru1999@gmail.com>
2026-05-10 06:31:00 +0300

    Update product: test

--- assets/js/cms-data.js
+++ assets/js/cms-data.js
@@ -4,9 +4,22 @@
 // Do not delete it. Public pages read this file globally.
 // ============================================================
 window.TENACITY_CMS_DATA = {
-  customProducts: [],
-  customCategories: [],
-  customBlog: [],
-  homepageSettings: {},
-  updatedAt: null
+  "customProducts": [
+    {
+      "id": "prod-1778383858896",
+      "name": "test",
+      "sku": "",
+      "category": "entry-level",
+      "image": "",
+      "price": 1,
+      "originalPrice": null,
+      "description": "",
+      "features": [],
+      "onOffer": false
+    }
+  ],
+  "customCategories": [],
+  "customBlog": [],
+  "homepageSettings": {},
+  "updatedAt": "2026-05-10T03:30:59.678Z"
 };
