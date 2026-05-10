cd534203c46763aaa48a3de6223f709bb0ae213e
Kimani Waweru <kimaniwaweru1999@gmail.com>
2026-05-10 06:34:40 +0300

    Delete product: prod-1778383858896

--- assets/js/cms-data.js
+++ assets/js/cms-data.js
@@ -4,22 +4,9 @@
 // Do not delete it. Public pages read this file globally.
 // ============================================================
 window.TENACITY_CMS_DATA = {
-  "customProducts": [
-    {
-      "id": "prod-1778383858896",
-      "name": "test",
-      "sku": "",
-      "category": "entry-level",
-      "image": "",
-      "price": 1,
-      "originalPrice": null,
-      "description": "",
-      "features": [],
-      "onOffer": false
-    }
-  ],
+  "customProducts": [],
   "customCategories": [],
   "customBlog": [],
   "homepageSettings": {},
-  "updatedAt": "2026-05-10T03:30:59.678Z"
+  "updatedAt": "2026-05-10T03:34:39.950Z"
 };
