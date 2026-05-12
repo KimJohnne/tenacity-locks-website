3aac341e4c2c92b5327f616195aacdef020ad7d6
Kimani Waweru <kimaniwaweru1999@gmail.com>
2026-05-12 16:14:12 +0300

    Update homepage settings

--- assets/js/cms-data.js
+++ assets/js/cms-data.js
@@ -4,9 +4,16 @@
 // Do not delete it. Public pages read this file globally.
 // ============================================================
 window.TENACITY_CMS_DATA = {
-  customProducts: [],
-  customCategories: [],
-  customBlog: [],
-  homepageSettings: {},
-  updatedAt: null
+  "customProducts": [],
+  "customCategories": [],
+  "customBlog": [],
+  "homepageSettings": {
+    "headline": "",
+    "subtext": "",
+    "promoTitle": "",
+    "promoText": "",
+    "email": "info@tenacitylocks.com",
+    "phone": "0711 77 66 88"
+  },
+  "updatedAt": "2026-05-12T13:14:12.129Z"
 };
