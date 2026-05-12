688a0739e0b01f61a9b6e0c26e4ac90f943c453e
Kimani Waweru <kimaniwaweru1999@gmail.com>
2026-05-12 17:48:44 +0300

    Update product: TM53 MSN Keyless Bathroom Lock

--- assets/js/cms-data.js
+++ assets/js/cms-data.js
@@ -6,6 +6,24 @@
 window.TENACITY_CMS_DATA = {
   "customProducts": [
     {
+      "id": "c38-ss-keyless",
+      "name": "TM53 MSN Keyless Bathroom Lock",
+      "sku": "TM53-MSN",
+      "category": "single-pin-ndk",
+      "image": "assets/images/products/c38-ss-keyless.jpg",
+      "price": 1500,
+      "originalPrice": 2499,
+      "description": "Premium TM53 MSN keyless bathroom lock. Sleek stainless look.",
+      "features": [
+        "No Key Required",
+        "TM53 MSN",
+        "Stainless Steel",
+        "CE Certified",
+        "5-year guarantee"
+      ],
+      "onOffer": false
+    },
+    {
       "id": "c38-black-keys",
       "name": "TM53 MCF - Keys Both Sides",
       "sku": "TM-53-MCF",
@@ -27,5 +45,5 @@ window.TENACITY_CMS_DATA = {
   "customCategories": [],
   "customBlog": [],
   "homepageSettings": {},
-  "updatedAt": "2026-05-12T14:46:41.302Z"
+  "updatedAt": "2026-05-12T14:48:43.628Z"
 };
